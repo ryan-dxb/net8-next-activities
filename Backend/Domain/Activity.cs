7ef42b453d67ea154092fce4f698619afaa9cee3
Ryan P <93515317+ryan-dxb@users.noreply.github.com>
2023-12-26 12:05:41 +0400

    Api Skeleton Done

---
+++ Backend/Domain/Activity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain;
+
+public class Activity
+{
+    [Key]  // This is the primary key. It is required if the name of the property is not Id or <classname>Id
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public DateTime Date { get; set; }
+    public string Description { get; set; }
+    public string Category { get; set; }
+    public string City { get; set; }
+    public string Venue { get; set; }
+
+}
